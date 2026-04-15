fff33d4d4d76799e20fb1ece8b07c42dcc5c631c
Marcelo Ayala <marcelorubenayala@outlook.com>
2026-04-15 18:34:54 -0300

    init: amazon cognito integration

---
+++ README.md
@@ -0,0 +1,6 @@
+<amplify-authenticator>
+  <ng-template amplifySlot="authenticated">
+    <h1>¡Bienvenido a Byte & Bite!</h1>
+    <button (click)="signOut()">Cerrar sesión</button>
+  </ng-template>
+</amplify-authenticator>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './amplifyconfiguration.json';
 
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
+// Configurar primero cognito
+Amplify.configure(amplifyconfig);
+
+// Arrancar la app después
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
