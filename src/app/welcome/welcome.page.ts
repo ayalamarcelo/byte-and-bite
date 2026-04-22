dcc2130622966826d0d0634a0bd42349909713b6
Marcelo Ayala <marcelorubenayala@outlook.com>
2026-04-22 01:52:24 -0300

    feat: implement navigation from welcome to tabs

--- src/app/welcome/welcome.page.ts
+++ src/app/welcome/welcome.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -8,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomePage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  goToHome() {
+    this.router.navigate(['/tabs/home']);
   }
 
 }
