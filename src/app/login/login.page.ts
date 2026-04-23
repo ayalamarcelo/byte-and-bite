fff82b54e2907114e7807ac6c518591f12514b2c
Marcelo Ayala <marcelorubenayala@outlook.com>
2026-04-23 17:56:49 -0300

    style(login): ajustes visuales en login y actualización de lógica de botones

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit() {
+  }
+
+  goToSignUp() {
+    this.router.navigate(['/signup']);
+  }
+
+  goToHome() {
+    this.router.navigate(['/tabs/home']);
   }
 
 }
