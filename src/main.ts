d73cfd1bb65e3b5b6d63d667b0b438c2c3e38fc8
Marcelo Ayala <marcelorubenayala@outlook.com>
2026-05-06 23:47:50 -0300

    Stop tracking environment files

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,22 @@ import  amplifyconfig  from './amplifyconfiguration.json';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
-// Configurar primero cognito
-Amplify.configure(amplifyconfig);
+// config cognito
 
-// Arrancar la app después
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: 'environment.cognito.userPoolId', 
+      userPoolClientId: 'environment.cognito.userPoolClientId',
+      loginWith: {
+        email: true,
+      }
+    }
+  }
+});
+
+// arranca la app después
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
