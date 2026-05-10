2117b3c17ad360bf5394bf70054cbcaac75a88c8
Thomas Gauna <thomy.gauna@gmail.com>
2026-05-10 17:20:59 -0300

    feat: Calendario de calorias consumidas y refactor de variables

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,17 +12,58 @@ import { IonicModule } from '@ionic/angular';
 })
 export class HomePage implements OnInit {
 
-  porcentajeGrasas: number = 30;
-  porcentajeProteinas: number = 60;
-  porcentajeCarbos: number = 10;
+  percentageFats: number = 30;
+  proteinPercentage: number = 60;
+  percentageCarbo: number = 10;
 
   sodioMg: number = 1500;
   fibraG: number = 25;
   potasioMg: number = 3500;
 
+  nameMonth: string = '';
+  daysOfTheMonth: any[] = [];
+  emptySpaces: number[] = [];
+  rightNow: number = new Date().getDate();
+
+ generarCalendario() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getFullYear() === 2026 && now.getMonth() === 4 ? 4 : now.getMonth();
+
+    const options: Intl.DateTimeFormatOptions = { month: 'long', year: 'numeric' };
+    this.nameMonth = now.toLocaleDateString('es-ES', options);
+
+    const numberOfDays = new Date(year, month + 1, 0).getDate();
+
+    const firstDayOfTheWeek = new Date(year, month, 1).getDay();
+    this.emptySpaces = Array(firstDayOfTheWeek).fill(0);
+
+    this.daysOfTheMonth = [];
+    for (let i = 1; i <= numberOfDays; i++) {
+      let consumptionExample = 0;
+      
+      if (i === 1) consumptionExample = 2100; 
+      if (i === 2) consumptionExample = 1500; 
+      if (i === 3) consumptionExample = 1500; 
+      if (i === 4) consumptionExample = 1500; 
+      if (i === 5) consumptionExample = 2300; 
+      if (i === 6) consumptionExample = 2300; 
+      if (i === 7) consumptionExample = 2400; 
+      if (i === 8) consumptionExample = 3400; 
+      if (i === 9) consumptionExample = 1200; 
+      this.daysOfTheMonth.push({
+        numero: i,
+        consumido: consumptionExample,
+        meta: 2000
+      });
+    }
+  }
+
   constructor() { }
 
   ngOnInit() {
+  this.generarCalendario();
   }
+  
 
 }
--- src/environments/environment-example.ts
+++ src/environments/environment-example.ts
@@ -1,7 +1,7 @@
-export const environment = {
+/*export const environment = {
   production: true,
   cognito: {
     userPoolId: 'User_Pool_ID', 
     userPoolClientId: 'User_Pool_Client_ID',
   }
-};
+};*/
